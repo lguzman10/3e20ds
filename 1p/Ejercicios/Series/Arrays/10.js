44b7c1786f741a69cb8f0a969346f77258931b0b
lguzman10 <72362832+lguzman10@users.noreply.github.com>
2020-11-18 13:43:13 -0600

    Delete 10.js

--- 1p/Ejercicios/Series/Arrays/10.js
+++
@@ -1,9 +0,0 @@
-//sen x
-
-let numeros = new Array(10);
-
-for (let i = 0; i < numeros.length; i++) 
-{
-    numeros[i] =  Math.sin(i);
-    console.log("El seno del número "+ i +" es: " + numeros[i]);
-}
