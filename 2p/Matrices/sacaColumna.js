4ca30a77e24265682f808ccb4c15c54e70367c51
lguzman10 <72362832+lguzman10@users.noreply.github.com>
2021-01-18 03:12:54 -0600

    Update sacaColumna.js

--- 2p/Matrices/sacaColumna.js
+++ 2p/Matrices/sacaColumna.js
@@ -1,1 +1,26 @@
+class Columna{
+    constructor(matriz, columna){
+        this.matriz = matriz;
+        this.columna = columna;
+    }
+
+    sacarColumna() {
+        let column = [];
+        for (let i = 0; i < this.matriz.length; i++) {
+            column.push(this.matriz[i][this.columna]);
+        }
+        return column;
+    }
+}
+
+
+let array = new Columna(
+    [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]],
+                        0);
+
+console.log(array.sacarColumna());
+
 
